859b53544124bfdd559af1f0915acfd81d4f6833
dremharrison <deandremharrison@gmail.com>
2019-07-18 18:17:12 -0400

    add json, pojo, butterknife and retrofit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivitys";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
