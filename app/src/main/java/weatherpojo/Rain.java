c29e5d053f82f2dfa481662750cc6473e003a4c5
dremharrison <deandremharrison@gmail.com>
2019-07-18 18:19:15 -0400

    add json, pojo, butterknife and retrofit

---
+++ app/src/main/java/weatherpojo/Rain.java
@@ -0,0 +1,27 @@
+package weatherpojo;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class Rain{
+
+	@SerializedName("1h")
+	private double jsonMember1h;
+
+	public void setJsonMember1h(double jsonMember1h){
+		this.jsonMember1h = jsonMember1h;
+	}
+
+	public double getJsonMember1h(){
+		return jsonMember1h;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Rain{" + 
+			"1h = '" + jsonMember1h + '\'' + 
+			"}";
+		}
+}
